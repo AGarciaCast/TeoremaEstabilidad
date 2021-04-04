6c8c40a67eb5341fc216bc63b289a159b753ff86
Alejandro <a.gcastellanos@alumnos.upm.es>
2021-04-04 18:31:39 +0200

    Avance background

--- tfg_latex/desarrollo.ps1
+++ tfg_latex/desarrollo.ps1
@@ -1,1 +1,2 @@
-pdflatex  -synctex=1 -interaction=nonstopmode --output-directory=desarrolloBuild -jobname=Desarrollo "\def\desarrollo{}\input{TFG_Teorema_Estabilidad}"
+cd C:\Users\Alejandro\Desktop\TFG\tfg_latex;
+pdflatex -synctex=1 -interaction=nonstopmode --output-directory=desarrolloBuild -jobname=Desarrollo "\def\desarrollo{}\input{TFG_Teorema_Estabilidad}";
