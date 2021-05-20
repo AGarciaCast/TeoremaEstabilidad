da8075295b7a4cabb1669da35034cdfae2e73884
Alejandro <a.gcastellanos@alumnos.upm.es>
2021-05-20 18:03:35 +0200

    Ejemplos y correcciones

--- code/distancias.py
+++ code/distancias.py
@@ -5,14 +5,14 @@ Created on Tue May  4 15:45:56 2021.
 @author: Alejandro
 """
 
-# import complejosSimpliciales
 import numpy as np
-import math
 import networkx as nx
 from networkx.algorithms import bipartite
 import matplotlib.pyplot as plt
-import gudhi
+import sympy as sy
 
+# variable curvas
+t = sy.symbols('t', real=True)
 
 
 def distanciaEuclidea(p1, p2):
@@ -26,6 +26,7 @@ def distanciaEuclidea(p1, p2):
     p2Np = np.array(p2)
     return np.sqrt(np.dot(p1Np - p2Np, p1Np - p2Np))
 
+
 def distanciaInf(x, y):
     """
     Calcular la distancia infiniro entre los puntos x e y.
@@ -36,7 +37,7 @@ def distanciaInf(x, y):
     res = 0
     if x[1] == float('inf') and y[1] == float('inf'):
         res = abs(x[0]-y[0])
-    elif x[1] ==  float('inf') and y[1] != float('inf'):
+    elif x[1] == float('inf') and y[1] != float('inf'):
         res = float('inf')
     elif x[1] != float('inf') and y[1] == float('inf'):
         res = float('inf')
@@ -108,13 +109,11 @@ def grafoBottleneck(X, Y):
     edges = list()
     for i in range(0, n):
         for j in range(0, n):
-            d = 0
+            d = 0.0
             if U[i] in X0 or V[j] in Y0:
                 d = distanciaInf(U[i], V[j])
 
-            # if d != float('inf'):
-
-            edges.append((f"u{i}", f"v{j}",d))
+            edges.append((f"u{i}", f"v{j}", d))
 
     G.add_weighted_edges_from(edges)
 
@@ -123,9 +122,10 @@ def grafoBottleneck(X, Y):
     if len(X0_inf) != len(Y0_inf):
         distPinf = float("inf")
     else:
-        distPinf = max([abs(x-y) for x,y in zip(sorted(X0_inf), sorted(Y0_inf))]+[0])
+        distPinf = max([abs(x-y) for x, y in zip(sorted(X0_inf), sorted(Y0_inf))]+[0])
 
     return G, U, V, distPinf
+
 
 def subgrafoG(G, i):
     G0 = nx.Graph([(u, v, d) for u, v, d in G.edges(data=True) if d['weight'] <= i])
@@ -134,99 +134,249 @@ def subgrafoG(G, i):
 
     return G0
 
-def cambiarPesos(G, Gi, dU, dV):
+
+def subgrafoG_todosV(G, i):
+    G0 = nx.Graph()
+    G0.add_nodes_from(G.nodes(data=True))
+    G0.add_edges_from([(u, v, d) for u, v, d in G.edges(data=True) if d['weight'] <= i])
+
+    return G0
+
+
+def cambiarPesos(Gi, length):
     for u, v, d in Gi.edges(data=True):
-        d['weight'] = G[u][v]['weight'] - dU[u] - dV[v]
+        d['weight'] += length[u] - length[v]
+        if d['weight'] < 0:
+            d['weight'] = 0
+
+    return 0
 
 
-def digrafoAsociado(G, M, U = None, V = None):
+def digrafoAsociado(G, M, U=None, V=None):
     if U is None or V is None:
         U, V = bipartite.sets(G)
 
     D = nx.DiGraph()
     keysM = M.keys()
-    D.add_weighted_edges_from([(u, v, d) if u not in keysM or M[u] != v
-                               else (v, u, d)
-                               for u, v, d in G.edges(data='weight')])
 
+    aristas = list()
+    for u, v, d in G.edges(data='weight'):
+        if u in U:
+            if u not in keysM or M[u] != v:
+                aristas.append((u, v, d))
+            else:
+                aristas.append((v, u, d))
+        else:
+            if u not in keysM or M[u] != v:
+                aristas.append((v, u, d))
+            else:
+                aristas.append((u, v, d))
 
+    D.add_weighted_edges_from(aristas)
     D.add_weighted_edges_from([("s", u, 0) for u in U if u not in keysM])
     D.add_weighted_edges_from([(v, "t", 0) for v in V if v not in keysM])
 
     return D
 
+
 def plotMatching(G, M, U):
     plt.figure(figsize=(8, 6))
     pos = nx.drawing.layout.bipartite_layout(G, U)
     nx.draw_networkx(G, pos=pos)
-    edgelist=[(u,M[u]) for u in U if u in M.keys()]
+    edgelist = [(u, M[u]) for u in U if u in M.keys()]
     nx.draw_networkx_edges(G, pos, edgelist=edgelist, width=2.5, edge_color='blue')
 
 
-def bottleneck(X, Y):
+def bottleneck(X, Y, plot=False, debug=False):
     G, _, _, distPinf = grafoBottleneck(X, Y)
     if distPinf == float("inf"):
         return float("inf")
     else:
         U, V = bipartite.sets(G)
-        dU = {u: 0 for u in U}
-        dV = {v: 0 for v in V}
         Gi = G.copy()
-        Gi_0 = subgrafoG(Gi, 0)
         Mi = dict()
 
-        plotMatching(Gi, Mi, U)
+        if plot:
+            plotMatching(Gi, Mi, U)
 
-        if Gi_0.nodes():
-            Mi = nx.bipartite.maximum_matching(Gi_0)
-
-        plotMatching(Gi, Mi, U)
-
-        while not nx.is_perfect_matching(Gi, Mi):
+        while not nx.is_perfect_matching(G, Mi):
             Di = digrafoAsociado(Gi, Mi, U, V)
             length, path = nx.single_source_dijkstra(Di, "s")
             caminoAumento = path["t"][1:-1]
 
-            for i in range(0, len(caminoAumento), 1):
+            for i in range(0, len(caminoAumento)):
                 x = caminoAumento[i]
-                # Mi.pop(x, None)
-                if i%2==0:
+
+                if i % 2 == 0:
                     Mi[x] = caminoAumento[i+1]
                 else:
                     Mi[x] = caminoAumento[i-1]
 
-            plotMatching(Gi, Mi, U)
+            if plot:
+                plotMatching(Gi, Mi, U)
 
-            for u in U:
-                dU[u] -= length[u]
+            if debug:
+                print("\n", {u: Mi[u] for u in U if u in Mi}, "\n")
+                print(nx.is_perfect_matching(subgrafoG_todosV(Gi, 0), Mi), "\n")
+                print("MAX", max([G[u][Mi[u]]["weight"] for u in U if u in Mi.keys()]+[distPinf]), "\n")
+                print(caminoAumento, "\n")
+                print([(u, v, d) for u, v, d in Gi.edges(data=True) if d["weight"] < 0], "\n")
 
-            for v in V:
-                dV[v] += length[v]
-
-            cambiarPesos(G, Gi, dU, dV)
-
+            cambiarPesos(Gi, length)
 
         return max([G[u][Mi[u]]["weight"] for u in U if u in Mi.keys()]+[distPinf])
 
-if __name__ == "__main__":
-    A = np.array([(2, 4), (3, 2), (0, 0), (0, 0.8), (4, float('inf'))])
-    B = np.array([(2.8, 4), (3, 3), (4.2, float('inf'))])
-    print("Hausdorff: ", hausdorffDir(A, B), "\n")
 
+if __name__ == "__main__":
+
+    A = np.array([(2, 4), (3, 2), (0, 0), (0, 0.8), (4, 5.2)])
+    B = np.array([(2.8, 4), (3, 3), (4.2, 5.8)])
+    print("Hausdorff: ", hausdorffDir(A, B), "\n")
+    """
     C = np.array([(2, 4), (4, 2), (0, 0)])
     D = np.array([(2.8, 4), (4.8, 2.8), (0, 0.8)])
 
     diag1 = np.array([(2.7, 3.7),(9.6, 14.),(34.2, 34.974), (3., float('inf'))])
     diag2 = np.array([(2.8, 4.45),(9.5, 14.1),(3.2, float('inf'))])
 
-    E = np.array([(0, 0)])
-    F = np.array([(0, 13)])
+    E = np.array(
+        [(0.0 ,               float('inf')),
+         (0.0 ,        1.13697226),
+         (0.0 ,        1.03039957),
+         (0.0 ,        0.95966316),
+         (0.0 ,        0.80698455),
+         (0.0 ,        0.80296038),
+         (0.0 ,        0.79696911),
+         (0.0 ,        0.79508741),
+         (0.0 ,        0.77841773),
+         (0.0 ,        0.76978861),
+         (0.0 ,        0.7203776 ),
+         (0.0 ,        0.6794018 ),
+         (0.0 ,        0.67052617),
+         (0.0 ,        0.65667931),
+         (0.0 ,        0.56198667),
+         (0.0 ,        0.50936598),
+         (0.0 ,        0.4636807 ),
+         (0.0 ,        0.42416795),
+         (0.0 ,        0.41190955),
+         (0.0 ,        0.32212969),
+         (0.0 ,        0.31169664),
+         (0.0   ,      0.27649719),
+         (0.0   ,      0.27640935),
+         (0.0   ,      0.25668194),
+         (0.0   ,      0.23450715),
+         (0.0   ,      0.20997965),
+         (0.0   ,      0.19962433),
+         (0.0   ,      0.18922214),
+         (0.0   ,      0.16333382),
+         (0.0   ,      0.00470359)]
+        )
+    F = np.array(
+        [(0.0,                float('inf')),
+         (0.0,         1.3321563 ),
+         (0.0,         1.31587497),
+         (0.0,         1.29301984),
+         (0.0,         1.23687585),
+         (0.0,         1.1775481 ),
+         (0.0,         0.99416275),
+         (0.0,         0.91404681),
+         (0.0,         0.82943645),
+         (0.0,         0.81629505),
+         (0.0,         0.80500498),
+         (0.0,         0.77720977),
+         (0.0,         0.70728105),
+         (0.0,         0.69391815),
+         (0.0,         0.61356143),
+         (0.0,         0.55564526),
+         (0.0,         0.54351151),
+         (0.0,         0.47907053),
+         (0.0,         0.4036146 ),
+         (0.0,         0.37371402),
+         (0.0,         0.36463867),
+         (0.0,         0.33523029),
+         (0.0,         0.32403681),
+         (0.0,         0.31127303),
+         (0.0,         0.18202163),
+         (0.0,         0.1645074 ),
+         (0.0,         0.11731804),
+         (0.0,         0.04215849),
+         (0.0,         0.02745002),
+         (0.0,         0.01426722)]
+            )
 
-    G, _, _, d = grafoBottleneck(diag1, diag2)
+    H=np.array(
+        [[0.0,                 float('inf')],
+         [0.0,         1.02396865],
+         [0.0,         1.01717134],
+         [0.0,         1.00700046],
+         [0.0,         0.99909419],
+         [0.0,         0.97990276],
+         [0.0,         0.88616666],
+         [0.0,         0.88399377],
+         [0.0,         0.79089903],
+         [0.0,        0.69375345],
+         [0.0,         0.66319856],
+         [0.0,        0.66229266],
+         [0.0,         0.6554447 ],
+         [0.0,         0.64426731],
+         [0.0,         0.55715821],
+         [0.0,         0.47581502],
+         [0.0,         0.44550496],
+         [0.0,         0.43900116],
+         [0.0,         0.34987359],
+         [0.0,         0.32084358],
+         [0.0,         0.31379172],
+         [0.0,         0.26364402],
+         [0.0,         0.25204839],
+         [0.0,         0.24996349],
+         [0.0,         0.24475344],
+         [0.0,         0.24279258],
+         [0.0,         0.2415098 ],
+         [0.0,         0.20149441],
+         [0.0,         0.15331565],
+         [0.0,         0.00733711]]
+
+        )
+
+    I= np.array(
+        [[0.0,                float('inf')],
+         [0.0,        2.094743  ],
+         [0.0,         1.7068323 ],
+         [0.0,         1.12945222],
+         [0.0,         0.9922288 ],
+         [0.0,         0.98077045],
+         [0.0,         0.97835381],
+         [0.0,         0.91853028],
+         [0.0,         0.85221723],
+         [0.0,         0.80193793],
+         [0.0,         0.76939726],
+         [0.0,         0.76232422],
+         [0.0,         0.74088451],
+         [0.0,         0.68880039],
+         [0.0,         0.59407499],
+         [0.0,         0.58294188],
+         [0.0,         0.51350641],
+         [0.0,         0.49164004],
+         [0.0,         0.40450249],
+         [0.0,         0.36180827],
+         [0.0,         0.30854649],
+         [0.0,         0.28261262],
+         [0.0,         0.26748562],
+         [0.0,         0.25891736],
+         [0.0,         0.22993343],
+         [0.0,         0.15913492],
+         [0.0,         0.14481686],
+         [0.0,         0.08865069],
+         [0.0,         0.0507751 ],
+         [0.0,         0.01062389]]
+            )
+
+    G, _, _, d = grafoBottleneck(H, I)
 
     print("DISTANCIA P INF", d)
     print(G.nodes(data=True), "\n")
     print(G.edges(data=True), "\n")
 
-    print(bottleneck(diag1, diag2), "=", gudhi.bottleneck_distance(diag1, diag2))
+    print(bottleneck(H,I), "=", gudhi.bottleneck_distance(H, I))
 
+    """
--- code/ejemplos.py
+++ code/ejemplos.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May 14 23:34:04 2021
+Created on Fri May 14 23:34:04 2021.
 
 @author: Alejandro
 """
@@ -8,71 +8,248 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import json
-import datetime
-
+import gudhi
 import folium
-from folium.plugins import TimeSliderChoropleth
+import sympy as sy
+import matplotlib.colors
+from folium.map import Layer, FeatureGroup,LayerControl,Marker
+import distancias
+from scipy.spatial.distance import directed_hausdorff
+
+# variable curvas
+t = sy.symbols('t', real=True)
+
+coloresPartidos = {
+                      "PSOE": "#DD5C47",
+                      "PP": "#5C95DA",
+                      "ERC": "#E6C410",
+                      "VOX": "#5CB451",
+                      "JxC": "#B6194D",
+                      "Bildu": "#6BE955",
+                      "PNV": "#4F954A",
+                      "NA+": "#A83B2A",
+                      "UP": "#8961AA",
+                      "PRC": "#269A1B",
+                      "CC": "#FFEF4B",
+                      "Cs": "#DF9B07",
+                      "TEX": "#037252"
+                      }
 
 
+def plotalpha(ac, st, k, ax):
+    """
+    Representar el alpha complejo del complejo K.
 
-m = folium.Map(
-    location=[40.416775, -3.703790],
-    tiles="cartodbpositron",
-    zoom_start=6,
-)
+    puntos: np.array.
+    K: Complejo.
+    """
+    puntos = np.array([ac.get_point(i) for i in range(st.num_vertices())])
 
+    triangulos = [s[0] for s in st.get_skeleton(2) if len(s[0]) == 3 and s[1] <= k]
+    if triangulos:
+        c = np.ones(len(puntos))
+        cmap = matplotlib.colors.ListedColormap("limegreen")
+        ax.tripcolor(puntos[:, 0], puntos[:, 1], [s[0] for s in st.get_skeleton(2) if len(s[0]) == 3 and s[1] <= k],
+                     c, edgecolor="k", lw=2, cmap=cmap)
 
+    ax.plot(puntos[:, 0], puntos[:, 1], 'ko')
 
-provincias_geo = r"spain_provincias.geojson"
-
-with open(provincias_geo) as provincias_file:
-    provincias_json = json.load(provincias_file)
-
-provincias = [provincias_json['features'][i]['properties']['texto']
-              for i in range(len(provincias_json['features']))]
-
-print(sorted(provincias))
-
-
-coloresPartidos={
-                  "PSOE": "#DD5C47",
-                  "PP": "#5C95DA",
-                  "ERC": "#E6C410",
-                  "VOX": "#5CB451",
-                  "JxC": "#B6194D",
-                  "Bildu": "#6BE955",
-                  "PNV": "#4F954A",
-                  "NA+": "#A83B2A",
-                  "UP": "#8961AA",
-                  "PRC": "#269A1B",
-                  "CC": "#FFEF4B",
-                  "Cs": "#DF9B07",
-                  "TEX": "#037252"
-                }
-
-elecciones = pd.read_csv('elecciones2019Nov.csv')
+    aristas = [s[0] for s in st.get_skeleton(2) if len(s[0]) == 2 and s[1] <= k]
+    if aristas:
+        for arista in aristas:
+            p1 = puntos[arista[0]]
+            p2 = puntos[arista[1]]
+            ax.plot([p1[0], p2[0]], [p1[1], p2[1]], 'k')
 
 
+def puntosCurvaRuido(curva, t, t0, t1, numPuntos=10, mu=0, sigma=0.1):
+    """
+    Obtener conjunto discretos de puntos de una curva con ruido.
 
-def estilo(feature):
+    curva: list.
+    t: t sympy symbol
+    t0: float
+        Inicio intervalo.
+    t1: float
+        Final intervalo.
+    numPuntos: int. Por defecto 10.
+    mu: float. Por defecto 0
+        Media para la distribución normal.
+    sigma: float. Por defecto 0.1
+        Desviación típica para la distribución normal.
+    """
+    valores = np.linspace(t0, t1, num=numPuntos)
+    puntosCurva = np.array([[x.subs(t, v) for x in curva] for v in valores], dtype=np.float64)
+    ruido = np.random.normal(mu, sigma, [numPuntos, len(curva)])
 
-    provincia=feature['properties']['texto']
-    print(provincia)
-    partido = elecciones.loc[elecciones["Provincia"]==provincia, "Partido"].values[0]
+    return puntosCurva + ruido
+
+
+def ejemploPlano(points1, points2, indice, mapa=None):
+    if mapa is None:
+        fig, axs = plt.subplots(2, 2, dpi=100)
+        fig.set_size_inches(15, 10)
+    else:
+        fig, axs = plt.subplots(1, 2, dpi=100)
+        fig.set_size_inches(15, 5)
+        sub_group1 = folium.FeatureGroup(name="Alpha Complejo Wikipedia", control=True, show=False)
+        sub_group2 = folium.FeatureGroup(name="Alpha Complejo Random", control=True, show=True)
+
+    alpha_complex1 = gudhi.AlphaComplex(points=points1)
+    simplex_tree1 = alpha_complex1.create_simplex_tree()
+
+    result_str = 'Alpha complex is of dimension ' + repr(simplex_tree1.dimension()) + ' - ' + \
+                 repr(simplex_tree1.num_simplices()) + ' simplices - ' + \
+                 repr(simplex_tree1.num_vertices()) + ' vertices.'
+    print(result_str)
+
+    value = max(set([filtered_value[1] for filtered_value in simplex_tree1.get_filtration()]))
+
+    if mapa is None:
+        plotalpha(alpha_complex1, simplex_tree1, value, axs[0, 0])
+        axs[0, 0].title.set_text(r"$r={}$".format(str(value)))
+    else:
+        plotAlphaMapa(alpha_complex1, simplex_tree1, value, mapa, sub_group1)
+
+    diag1 = simplex_tree1.persistence()
+
+    if mapa is None:
+        gudhi.plot_persistence_diagram(diag1, axes=axs[0, 1])
+    else:
+        gudhi.plot_persistence_diagram(diag1, axes=axs[0])
+
+    alpha_complex2 = gudhi.AlphaComplex(points=points2)
+    simplex_tree2 = alpha_complex2.create_simplex_tree()
+
+    result_str = 'Alpha complex is of dimension ' + repr(simplex_tree2.dimension()) + ' - ' + \
+                 repr(simplex_tree2.num_simplices()) + ' simplices - ' + \
+                 repr(simplex_tree2.num_vertices()) + ' vertices.'
+    print(result_str)
+
+    value = max(set([filtered_value[1] for filtered_value in simplex_tree1.get_filtration()]))
+
+    if mapa is None:
+        plotalpha(alpha_complex2, simplex_tree2, value, axs[1, 0])
+        axs[1, 0].title.set_text(r"$r={}$".format(str(value)))
+    else:
+        plotAlphaMapa(alpha_complex2, simplex_tree2, value, mapa, sub_group2)
+
+    diag2 = simplex_tree2.persistence()
+    if mapa is None:
+        gudhi.plot_persistence_diagram(diag2, axes=axs[1, 1])
+    else:
+        gudhi.plot_persistence_diagram(diag2, axes=axs[1])
+
+    fig.tight_layout()
+    fig.savefig(f'ejemplo{indice}.png', dpi=100)
+    plt.show()
+
+    diag1_0 = np.array([s[1] for s in diag1 if s[0] == 0])
+    diag2_0 = np.array([s[1] for s in diag2 if s[0] == 0])
+
+    print("\nDistancia Hausdorff: ", round(distancias.hausdorffDir(points1, points2), 5))
+
+    print("Distancia bottleneck dimensión 0:", round(distancias.bottleneck(diag1_0, diag2_0), 5),
+          "(Implementación GUDHI:", str(round(gudhi.bottleneck_distance(diag1_0, diag2_0), 5)) + ")")
+
+    diag1_1 = np.array([s[1] for s in diag1 if s[0] == 1])
+    diag2_1 = np.array([s[1] for s in diag2 if s[0] == 1])
+    print("Distancia bottleneck dimensión 1:", round(distancias.bottleneck(diag1_1, diag2_1), 5),
+          "(Implementación GUDHI:", str(round(gudhi.bottleneck_distance(diag1_1, diag2_1), 5)) + ")", "\n")
+
+
+def ejemplo1(backup=True):
+    if backup:
+        with open('puntos1_1.npy', 'rb') as f:
+            points1 = np.load(f)
+
+        with open('puntos1_2.npy', 'rb') as f:
+            points2 = np.load(f)
+    else:
+        curva = [4 * sy.sin(t), 9 * sy.cos(t)]
+        points1 = puntosCurvaRuido(curva, t, 0, 2*np.pi, numPuntos=30)
+        points2 = puntosCurvaRuido(curva, t, 0, 2*np.pi, numPuntos=30, mu=2, sigma=0.3)
+
+    print("-------------Ejemplo 1-------------\n")
+    ejemploPlano(points1, points2, 1)
+
+
+def ejemplo2(backup=True):
+    if backup:
+        with open('puntos2_1.npy', 'rb') as f:
+            points1 = np.load(f)
+
+        with open('puntos2_2.npy', 'rb') as f:
+            points2 = np.load(f)
+    else:
+        curva = [10 * sy.cos(2 * t) * sy.cos(t),
+                 10 * sy.cos(2 * t) * sy.sin(t)]
+        points1 = puntosCurvaRuido(curva, t, 0, 2*np.pi, numPuntos=50, sigma=0.00)
+        points2 = puntosCurvaRuido(curva, t, 0, 2*np.pi, numPuntos=50, mu=2, sigma=0.3)
+
+    print("-------------Ejemplo 2-------------\n")
+    ejemploPlano(points1, points2, 2)
+
+
+def estiloMapa(feature, elecciones):
+    provincia = feature['properties']['texto']
+    partido = elecciones.loc[elecciones["Provincia"] == provincia, "Partido"].values[0]
     provincia_style = {
-             'fillOpacity': 1,
-             'weight': 1,
-             'fillOpacity': 0.8,
-             'fillColor': coloresPartidos[partido],
-             'color': '#000000'}
+                        'weight': 1,
+                        'fillOpacity': 0.8,
+                        'fillColor': coloresPartidos[partido],
+                        'color': '#000000'
+                      }
 
     return provincia_style
 
-folium.GeoJson(provincias_geo, name="geojson", style_function=estilo).add_to(m)
 
-folium.CircleMarker([41.583333, -4.666667], radius=4, fill=True, fill_opacity=1, fill_color='white',
-    color = 'black').add_to(m)
+def plotAlphaMapa(ac, st, k, m, sub_group):
+    """
+    Representar el alpha complejo del complejo K.
 
-m.save("mapaEspaña.html")
+    puntos: np.array.
+    K: Complejo.
+    """
+    puntos = np.array([ac.get_point(i) for i in range(st.num_vertices())])
+    aristas = [s[0] for s in st.get_skeleton(2) if len(s[0]) == 2 and s[1] <= k]
+
+    if aristas:
+        for arista in aristas:
+            p1 = puntos[arista[0]].tolist()
+            p2 = puntos[arista[1]].tolist()
+            folium.PolyLine([p1, p2], weight=3, color='black').add_to(sub_group)
+
+    for p in puntos:
+        folium.CircleMarker(p.tolist(), radius=4, fill=True, fill_opacity=1,
+                            fill_color='white', color='black').add_to(sub_group)
+    sub_group.add_to(m)
 
 
+def ejemploMapa():
+    m = folium.Map(
+        location=[36.2, -4],
+        tiles="CartoDB Positron",
+        zoom_start=6,
+    )
+
+    elecciones = pd.read_csv('elecciones2019Nov.csv')
+    eleccionesPSOE = elecciones[elecciones["Partido"] == "PSOE"]
+    coordenadasWiki = np.array([tuple(elem) for elem in eleccionesPSOE[["Latitud", "Longitud"]].values.tolist()])
+    coordenadasRandom = np.array([tuple(elem) for elem in eleccionesPSOE[["LatitudR", "LongitudR"]].values.tolist()])
+
+    folium.GeoJson(r"spain_provincias.geojson", name="Partidos Ganadores", control=False,
+                   style_function=(lambda feature: estiloMapa(feature, elecciones))).add_to(m)
+
+    print("-------------Ejemplo 3-------------\n")
+    ejemploPlano(coordenadasWiki, coordenadasRandom, "Mapa", mapa=m)
+
+    folium.LayerControl(collapsed=False).add_to(m)
+    m.save("mapaEspaña.html")
+    return elecciones, coordenadasRandom
+
+
+if __name__ == "__main__":
+    ejemplo1()
+    ejemplo2()
+    elecciones, coordenadasRandom = ejemploMapa()
